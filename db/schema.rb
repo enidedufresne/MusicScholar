24399296b4cfd891e1b28470c362a162e3085caf
Enide Dufresne <enide.dufresne@gmail.com>
2015-02-24 10:54:21 -0500

    update to schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,10 @@
 ActiveRecord::Schema.define(version: 20150224135757) do
 
   create_table "categories", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "players", force: :cascade do |t|
