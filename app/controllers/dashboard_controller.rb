1b7c0fdc2c5258d63b92571e2444bc0baf2cf4dd
Enide Dufresne <enide.dufresne@gmail.com>
2015-02-25 19:02:14 -0500

    questions view page with YouTube video

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
 	before_filter :authenticate_player!
 
 	def index
-		
+		@question = { youtube_id: 'kt0g4dWxEBo', options: ["1", "2", "3"]}
 	end
 end
