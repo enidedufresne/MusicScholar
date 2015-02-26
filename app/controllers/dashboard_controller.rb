bb3075e7497468f37e3bc2ce0af083970aa7fd6c
Alex Moraleza <Alexanderbusta@hotmail.com>
2015-02-26 14:05:46 -0500

    Merge completed

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,17 +1,13 @@
 class DashboardController < ApplicationController
 	before_filter :authenticate_player!
 
-<<<<<<< HEAD
-	def index	
+	def index
+		@question = { youtube_id: 'kt0g4dWxEBo', options: ["1", "2", "3"]}
 	end
 
 	def new
 	end
 
 	def edit
-=======
-	def index
-		@question = { youtube_id: 'kt0g4dWxEBo', options: ["1", "2", "3"]}
->>>>>>> b1a3b8ff2e7f1165296eb2c48ef5eae951bd39c4
 	end
 end
