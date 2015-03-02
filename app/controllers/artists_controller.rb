52b0dfc3aca0ab83d3a0d2d728f5b3eef21ab506
Alex Moraleza <Alexanderbusta@hotmail.com>
2015-03-02 16:19:37 -0500

    players now able to answer questions

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -12,11 +12,22 @@ class ArtistsController < ApplicationController
 		@question = @artist.questions.first
 	end
 
-	 def create
+	def create
   	end
 
-  def participant
-  end
+	def participant
+	end
+
+	# post '/responses' => 'artists#create_response', as: :response
+	def create_response
+		@response = Response.create(player_id: current_player.id, option_id: params[:option])
+		if @response.option.correct
+			redirect_to :back
+		else
+			artist = @response.option.question.artist
+			redirect_to category_path(artist.category), :notice => "Sorry try again!"
+		end
+	end
 
 	def edit
 	end
