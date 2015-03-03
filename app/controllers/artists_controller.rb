dd42fd3be392171d08cdcaa433d5760865fba817
Alex Moraleza <Alexanderbusta@hotmail.com>
2015-03-02 19:17:34 -0500

    After player answer's correct question it redirects to next question

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -9,7 +9,7 @@ class ArtistsController < ApplicationController
 
 	def show
 		@artist = Artist.find(params[:id])
-		@question = @artist.questions.first
+		@question = @artist.questions.where.not(id: params[:last_question]).order('random() asc').take
 	end
 
 	def create
@@ -21,8 +21,10 @@ class ArtistsController < ApplicationController
 	# post '/responses' => 'artists#create_response', as: :response
 	def create_response
 		@response = Response.create(player_id: current_player.id, option_id: params[:option])
+		@question = @response.option.question
 		if @response.option.correct
-			redirect_to :back
+			redirect_to category_artist_path(last_question: @question.id,
+			category_id: @question.artist.category.id, id: @question.artist.id)
 		else
 			artist = @response.option.question.artist
 			redirect_to category_path(artist.category), :notice => "Sorry try again!"
